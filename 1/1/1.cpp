a42ba15913f846d4d8a9f20329d56de80d2e27e5
Igor Kastyrkin <kastyrkinIV@ya.ru>
2023-11-06 19:17:39 +0300

    initial commit

---
+++ 1/1/1.cpp
@@ -0,0 +1,56 @@
+﻿
+
+#include <iostream>
+#include <fstream>
+
+class LogCommand {
+public:
+    virtual ~LogCommand() = default;
+    virtual void print(const std::string& message) = 0;
+
+};
+
+
+class LogConsole :public LogCommand {
+public:
+
+    void print(const std::string& messege) override
+    {
+        std::cout << messege << std::endl;
+    }
+};
+
+
+class LogFile :public LogConsole {
+    std::string filename;
+    std::fstream f;
+public:
+    LogFile(std::string fName) : filename(fName)
+    {
+        f.open(fName, std::fstream::in | std::fstream::out | std::fstream::app);
+        if (!(f.is_open())) std::cout << "can't open file" << std::endl;
+        else std::cout << "File opend"<< std::endl;
+    }
+    void print(const std::string& messege) override
+    {
+        f << messege << std::endl;
+        f.close();
+        std::cout << "file updated and closed" << std::endl;
+    }
+};
+
+void print(LogCommand& L, std::string text)
+{
+    L.print(text);
+}
+
+int main()
+{
+    LogConsole LC;
+    LogFile LF("example.txt");
+    print(LC, "Data1");
+    print(LF, "Data2");
+
+
+}
+
---
+++ 2/2/2.cpp
@@ -0,0 +1,79 @@
+﻿
+
+
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::move;
+
+class observer {
+public:
+    virtual void onWarning(const std::string& message) {
+        cout << message << endl;
+    }
+    virtual void onError(const std::string& message) { cout << message << endl; }
+    virtual void onFatalError(const std::string& message) { cout << message << endl; }
+};
+
+
+class observed {
+    vector<observer*> obj;
+public:
+    void warning(string message)const
+    {
+        for (auto m : obj)
+        {
+            m->onWarning(message);
+        }
+    }
+
+    void error(string message)const
+    {
+        for (auto m : obj)
+        {
+            m->onError(message);
+        }
+    }
+
+    void fatalError(string message)const
+    {
+        for (auto m : obj)
+        {
+            m->onFatalError(message);
+        }
+    }
+
+    void addObserver(observer* obs)
+    {
+        obj.push_back(obs);
+    }
+
+};
+
+
+int main()
+{
+    
+    observer* ov;
+    ov = new observer;
+    observer* od;
+    od = new observer;
+
+    observed ob;
+    ob.addObserver(ov);
+    ob.addObserver(od);
+
+    ob.warning("warning text");
+    ob.error("error text");
+    ob.fatalError("fatal error text");
+
+}
+
